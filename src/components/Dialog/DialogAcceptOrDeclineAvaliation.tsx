9119cd38bc6e63c0aa6da2aa6efca018525e5469
carlosrobert0 <carlosroberto15755@gmail.com>
2023-05-09 23:58:08 -0300

    Creating dialog accept or decline avaliation

---
+++ src/components/Dialog/DialogAcceptOrDeclineAvaliation.tsx
@@ -0,0 +1,154 @@
+import { Dialog, Transition } from "@headlessui/react";
+import Image from 'next/image';
+import { Fragment } from "react";
+import { FaStar } from "react-icons/fa";
+import { FiStar, FiX } from "react-icons/fi";
+
+interface DialogAcceptOrDeclineAvaliation {
+  isOpen: boolean;
+  onClose: () => void;
+  place_name: string | string[];
+  city_name: string | null;
+  category_name: string | null
+}
+
+interface FormData {
+  name: string,
+  avatar: string,
+  description: string,
+  status: string,
+  avaliation: number
+}
+
+export function DialogAcceptOrDeclineAvaliation({
+  isOpen,
+  onClose,
+  place_name,
+  city_name,
+  category_name
+}: DialogAcceptOrDeclineAvaliation) {
+  // async function handleUpdateDeposition({
+  //   name, avatar = imageData, description, status = 'accept',
+  //   avaliation = stars
+  // }: FormData) {
+  //   try {
+  //     await api.post('depositions', {
+  //       name,
+  //       avatar,
+  //       description,
+  //       status,
+  //       avaliation,
+  //       city_id,
+  //       place_id
+  //     })
+  //     setImageData('')
+  //     setStars(0)
+  //     reset()
+  //     onClose()
+  //     openModalAvaliationSent()
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={onClose}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-title bg-opacity-95" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 pt-0 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="relative flex h-[465px] overflow-hidden w-[736px] 
+          transform flex-col items-center justify-start transition-all bg-background rounded-[20px] mt-16"
+                >
+                  <header
+                    className="w-full h-[96px] bg-white overflow-hidden px-10 items-center justify-between flex rounded-t-[20px] border border-b-shape_secondary"
+                  >
+                    <h2 className='font-barlow font-semibold text-2xl leading-9 text-brand-orange'>Nota 5</h2>
+                    <button onClick={onClose} className="w-10 h-10 rounded-[10px] bg-shape border border-shape_secondary flex items-center justify-center">
+                      <FiX size={20} color="#AOACB2" />
+                    </button>
+                  </header>
+                  <div
+                    // onSubmit={handleSubmit(handleCreateDeposition)} 
+                    className='w-full px-10 flex flex-wrap justify-start mt-10 gap-4 mb-0'>
+                    <div className="flex items-center justify-start flex-col gap-6">
+                      <div className="w-[626px] h-[118px] flex gap-6 items-start">
+                        <Image
+                          src="/imgComment.png"
+                          width="64px"
+                          height="64px"
+                          objectFit="contain"
+                          className="rounded-full"
+                        />
+                        <div className="flex flex-col gap-2 items-start justify-center">
+                          <h3 className='font-barlow font-semibold text-xl leading-[26px] text-text'>Maria Eduarda</h3>
+                          <p className='font-heebo text-base leading-[26px] text-text w-[538px] text-left'>Todos os produtos comercializados são de excelente qualidade, recomendo!</p>
+                          <div className="flex gap-2">
+                            <FaStar size={20} color="#F25D27" />
+                            <FaStar size={20} color="#F25D27" />
+                            <FaStar size={20} color="#F25D27" />
+                            <FaStar size={20} color="#F25D27" />
+                            <FiStar size={20} color="#F25D27" />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex mb-10 ml-5 gap-[86px]">
+                        <div className="flex gap-10">
+                          <div className="flex flex-col items-start justify-between">
+                            <p className="font-heebo font-medium text-[10px] leading-[22px] text-text uppercase">CATEGORIA</p>
+                            <h2 className="font-heebo font-medium text-base leading-[26px] text-text">{category_name}</h2>
+                          </div>
+                          <div className="flex flex-col items-start justify-between">
+                            <p className="font-heebo font-medium text-[10px] leading-[22px] text-text uppercase">CIDADE</p>
+                            <h2 className="font-heebo font-medium text-base leading-[26px] text-text">{city_name}</h2>
+                          </div>
+                        </div>
+                        <div className="flex flex-col items-start justify-between">
+                          <p className="font-heebo font-medium text-[10px] leading-[22px] text-text uppercase">LOCAL</p>
+                          <h2 className="font-heebo font-medium text-base leading-[26px] text-text">{place_name}</h2>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <footer
+                    className="w-full mt-auto h-[96px] bg-white overflow-hidden px-10 items-center justify-between flex rounded-b-[20px] border border-b-shape_secondary"
+                  >
+                    <div className="flex gap-2 ml-auto">
+                      <button onClick={onClose} className="w-[123px] h-12 rounded-[10px] bg-attention font-heebo font-medium text-base leading-[26px] text-shape flex items-center justify-center">
+                        Recusar
+                      </button>
+                      <button onClick={onClose} className="w-[123px] h-12 rounded-[10px] bg-success font-heebo font-medium text-base leading-[26px] text-shape flex items-center justify-center">
+                        Aceitar
+                      </button>
+                    </div>
+                  </footer>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
--- src/pages/comments/index.tsx
+++ src/pages/comments/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { parseCookies } from "nookies";
 import { useEffect, useState } from "react";
+import { DialogAcceptOrDeclineAvaliation } from "../../components/Dialog/DialogAcceptOrDeclineAvaliation";
 import Nav from "../../components/Nav";
 import { useAuth } from "../../contexts/AuthContext";
 import { api } from "../../services/api";
@@ -11,8 +12,8 @@ interface CommentData {
   avatar?: string
   description?: string
   status: string
-  city_id: string
-  place_id: string
+  city: CityData
+  place: any
 }
 
 interface CityData {
@@ -24,7 +25,10 @@ interface CityData {
 
 export default function Comments() {
   const [comments, setComments] = useState<CommentData[]>([])
-  const [cities, setCities] = useState<CityData[]>([])
+  const [isOpen, setIsOpen] = useState(false)
+  const [placeNameComment, setPlaceNameComment] = useState('')
+  const [cityNameComment, setCityNameComment] = useState('')
+  const [categoryNameComment, setCategoryNameComment] = useState('')
 
   const router = useRouter()
 
@@ -32,134 +36,104 @@ export default function Comments() {
 
   const { signOutApplication } = useAuth()
 
+  function openDialogAcceptOrDeclineAvaliation() {
+    setIsOpen(true)
+  }
+
+  function closeDialogAcceptOrDeclineAvaliation() {
+    setIsOpen(false)
+  }
+
   async function getComments() {
     try {
       const response = await api.get('depositions')
+      console.log(response.data)
       setComments(response.data)
     } catch (error) {
       console.log(error)
     }
   }
 
-  async function getCities() {
-    try {
-      const response = await api.get('cities', {
-        headers: {
-          Authorization: `Bearer ${cookies['caparao.token']}`,
-        },
-      })
-      setCities(response.data)
-    } catch (error) {
-      if (error.response.status === 401) {
-        signOutApplication(router)
-      }
-    }
-  }
-
-  function renderCityName(city_id: string) {
-    const city = cities.filter(c => c.id === city_id)
-    return city[0]?.name
-  }
-
   useEffect(() => {
     getComments()
-    getCities()
   }, [])
 
   return (
-    <div className="relative">
-      <Nav />
-      <header className="ml-24 bg-shape h-24 w-full flex items-center justify-between px-28">
-        <h1 className="font-barlow text-4xl leading-[46px] text-title font-semibold">Comentários</h1>
+    <>
+      <div className="relative">
+        <Nav />
+        <header className="ml-24 bg-shape h-24 w-full flex items-center justify-between px-28">
+          <h1 className="font-barlow text-4xl leading-[46px] text-title font-semibold">Comentários</h1>
 
-        <ul className="flex gap-8 mr-24 text-complement text-base leading-[26px] font-roboto">
-          <li className="text-title font-bold border-b-2 border-brand-orange">Todos</li>
-          <li>Recentes</li>
-          <li>Velhos</li>
-          <li>Aceitos</li>
-          <li>Recusados</li>
-        </ul>
+          <ul className="flex gap-8 mr-24 text-complement text-base leading-[26px] font-roboto">
+            <li className="text-title font-bold border-b-2 border-brand-orange">Todos</li>
+            <li>Recentes</li>
+            <li>Velhos</li>
+            <li>Aceitos</li>
+            <li>Recusados</li>
+          </ul>
 
-      </header>
-      <main className="ml-[208px] mt-12 flex items-center">
-        <table className="w-[1120px] border-separate border-spacing-y-2 max-h-[760px] overflow-y-scroll">
-          <tr
-            className="w-full h-[88px] bg-shape">
-            <td className="pl-6 rounded-tl-2xl rounded-bl-2xl">
-              <div className="flex items-center justify-center gap-6">
-                <img src="/caparao.jpg" alt="" className="object-cover w-14 h-14 rounded-[50%]" />
-                <h4>Tiago Luchtenbeerg</h4>
-              </div>
-            </td>
-            <td width="7%">
-              <div className="bg-[#F3F3F3] rounded-[50%] h-6 w-6 flex items-center justify-center">
-                <img src="/interrogation.svg" alt="" />
-              </div>
-            </td>
-            <td>
-              <div className="flex flex-col">
-                <h4 className="font-heebo font-medium text-[10px] leading-[22px] text-complement">CATEGORIA</h4>
-                <h3 className="font-heebo font-medium text-base leading-6 text-text">Eventos Organizados</h3>
-              </div>
-            </td>
-            <td>
-              <div className="flex flex-col">
-                <h4 className="font-heebo font-medium text-[10px] leading-[22px] text-complement">CIDADE</h4>
-                <h3 className="font-heebo font-medium text-base leading-6 text-text">Rio do Sul</h3>
-              </div>
-            </td>
-            <td>
-              <div className="flex flex-col">
-                <h4 className="font-heebo font-medium text-[10px] leading-[22px] text-complement">LOCAL</h4>
-                <h3 className="font-heebo font-medium text-base leading-6 text-text">Rua Doce dos Encantos</h3>
-              </div>
-            </td>
-            <td className="pr-[38px] rounded-tr-2xl rounded-br-2xl">
-              <img src="/seta.svg" alt="" />
-            </td>
-          </tr>
-          {comments.map(comment => (
-            <tr
-              key={comment.id}
-              className="w-full h-[88px] bg-shape">
-              <td className="pl-6 rounded-tl-2xl rounded-bl-2xl">
-                <div className="flex items-center justify-center gap-6">
-                  <img src={comment.avatar} alt="" className="object-cover w-14 h-14 rounded-[50%]" />
-                  <h4>{comment.name}</h4>
-                </div>
-              </td>
-              <td width="7%">
-                <div className="bg-[#F3F3F3] rounded-[50%] h-6 w-6 flex items-center justify-center">
-                  <img src="/interrogation.svg" alt="" />
-                </div>
-              </td>
-              <td>
-                <div className="flex flex-col">
-                  <h4 className="font-heebo font-medium text-[10px] leading-[22px] text-complement">CATEGORIA</h4>
-                  <h3 className="font-heebo font-medium text-base leading-6 text-text">Eventos Organizados</h3>
-                </div>
-              </td>
-              <td>
-                <div className="flex flex-col">
-                  <h4 className="font-heebo font-medium text-[10px] leading-[22px] text-complement">CIDADE</h4>
-                  <h3 className="font-heebo font-medium text-base leading-6 text-text">
-                    {renderCityName(comment.city_id)}
-                  </h3>
-                </div>
-              </td>
-              <td>
-                <div className="flex flex-col">
-                  <h4 className="font-heebo font-medium text-[10px] leading-[22px] text-complement">LOCAL</h4>
-                  <h3 className="font-heebo font-medium text-base leading-6 text-text">{comment.place_id}</h3>
-                </div>
-              </td>
-              <td className="pr-[38px] rounded-tr-2xl rounded-br-2xl">
-                <img src="/seta.svg" alt="" />
-              </td>
-            </tr>
-          ))}
-        </table>
-      </main>
-    </div>
+        </header>
+        <main className="ml-[208px] mt-12 flex items-center">
+          <table className="w-[1120px] border-separate border-spacing-y-2 max-h-[760px] overflow-y-scroll">
+            {comments.map(comment => (
+              <tr
+                key={comment.id}
+                className="w-full h-[88px] bg-shape border border-red-900">
+                <td className="pl-6 rounded-tl-2xl rounded-bl-2xl">
+                  <div className="flex items-center justify-center gap-6">
+                    <img src="/caparao.jpg" alt="" className="object-cover w-14 h-14 rounded-[50%]" />
+                    <h4>{comment.name}</h4>
+                  </div>
+                </td>
+                <td width="7%">
+                  <div className="bg-[#F3F3F3] rounded-[50%] h-6 w-6 flex items-center justify-center">
+                    <img src="/interrogation.svg" alt="" />
+                  </div>
+                </td>
+                <td>
+                  <div className="flex flex-col">
+                    <h4 className="font-heebo font-medium text-[10px] leading-[22px] text-complement">CATEGORIA</h4>
+                    <h3 className="font-heebo font-medium text-base leading-6 text-text">{comment.place?.category.name}</h3>
+                  </div>
+                </td>
+                <td>
+                  <div className="flex flex-col">
+                    <h4 className="font-heebo font-medium text-[10px] leading-[22px] text-complement">CIDADE</h4>
+                    <h3 className="font-heebo font-medium text-base leading-6 text-text">
+                      {comment.city.name}
+                    </h3>
+                  </div>
+                </td>
+                <td>
+                  <div className="flex flex-col">
+                    <h4 className="font-heebo font-medium text-[10px] leading-[22px] text-complement">LOCAL</h4>
+                    <h3 className="font-heebo font-medium text-base leading-6 text-text">{comment.place.name}</h3>
+                  </div>
+                </td>
+                <td className="pr-[38px] rounded-tr-2xl rounded-br-2xl">
+                  <button onClick={() => {
+                    openDialogAcceptOrDeclineAvaliation()
+                    setCityNameComment(comment.city.name)
+                    setPlaceNameComment(comment.place.name)
+                    setCategoryNameComment(comment.place?.category.name)
+                  }
+                  }>
+                    <img src="/seta.svg" alt="" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </table>
+        </main>
+      </div>
+      <DialogAcceptOrDeclineAvaliation
+        isOpen={isOpen}
+        onClose={closeDialogAcceptOrDeclineAvaliation}
+        city_name={cityNameComment}
+        place_name={placeNameComment}
+        category_name={categoryNameComment}
+      />
+    </>
   )
 }
