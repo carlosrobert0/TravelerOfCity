edaf3d9b69b9bddddb3de8d8cd19d6f4f1327816
carlosrobert0 <carlosroberto15755@gmail.com>
2024-02-19 23:40:19 -0300

    feat: adding responsividade in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,8 +38,8 @@ export default function Home() {
   }, [])
 
   return (
-    <div className="h-full max-h-[820px] w-full overflow-hidden bg-background px-[160px]">
-      <header className="mt-6 flex justify-between">
+    <div className="h-full md:max-h-[820px] w-full overflow-hidden bg-background px-8 md:px-[160px]">
+      <header className="mt-8 md:mt-6 flex justify-between">
         <img src="/traveler.svg" alt="" />
         <Link href="/login">
           <button className="h-12 w-[174px] rounded-[10px] bg-blue_light font-heebo text-base font-medium leading-[26px] text-brand-blue hover:opacity-90">
@@ -47,9 +47,9 @@ export default function Home() {
           </button>
         </Link>
       </header>
-      <main className="flex flex-row gap-[150px]">
-        <aside className="mt-[138px] flex flex-col gap-10">
-          <h2 className="w-[317px] font-barlow text-[80px] font-semibold leading-[74px] text-title">
+      <main className="flex flex-col md:flex-row gap-24 md:gap-[150px]">
+        <aside className="mt-16 md:mt-[138px] flex flex-col md:gap-10 gap-14">
+          <h2 className="w-[317px] font-barlow text-6xl md:text-[80px] font-semibold leading-12 md:leading-[74px] text-title">
             Viva uma grande aventura
           </h2>
           <h3 className="w-[329px] font-heebo text-[20px] font-normal leading-[30px] text-text">
@@ -57,12 +57,12 @@ export default function Home() {
             Minas Gerais e do Brasil.
           </h3>
           <Link href="/cities/list">
-            <button className="mt-2 h-[72px] w-[329px] rounded-[10px] bg-brand-orange font-heebo text-lg font-medium leading-[26px] text-shape">
+            <button className="mt-6 hidden md:visible md:mt-2 h-[72px] w-[329px] mx-auto md:mx-0 rounded-[10px] bg-brand-orange font-heebo text-lg font-medium leading-[26px] text-shape">
               Descobrir todos os lugares
             </button>
           </Link>
         </aside>
-        <article className="mt-4 flex w-full gap-8">
+        <article className="md:mt-4 -mt-6 flex-col md:flex ml-5 w-full gap-8">
           <div className="flex flex-col gap-8">
             {cities.slice(0, 3).map((city) => (
               <CardCity
@@ -76,7 +76,7 @@ export default function Home() {
               />
             ))}
           </div>
-          <div className="mt-[75px] flex flex-col gap-8">
+          <div className="mt-[75px] md:flex flex-col gap-8 hidden">
             {cities.slice(4, 6).map((city) => (
               <CardCity
                 key={city.id}
@@ -89,6 +89,11 @@ export default function Home() {
             ))}
           </div>
         </article>
+        <Link href="/cities/list">
+            <button className="mt-6 md:hidden mb-10 md:mb-0 md:mt-2 h-[72px] w-[329px] mx-auto md:mx-0 rounded-[10px] bg-brand-orange font-heebo text-lg font-medium leading-[26px] text-shape">
+              Descobrir todos os lugares
+            </button>
+          </Link>
       </main>
     </div>
   )
